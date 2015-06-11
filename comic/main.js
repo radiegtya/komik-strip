7efa99dc8695bbac7c7ae3bdbbfb742feb682189
radiegtya <radiegtya@yahoo.co.id>
2015-06-11 11:40:20 +0700

    comic view done

--- comic/main.js
+++ comic/main.js
@@ -1,70 +1,153 @@
+var moment = require('moment');
+
 var MARGIN_SMALL = 4;
 var MARGIN = 8;
 
-var titleCompY = 0;
-
 var page = tabris.create("Page", {
-  topLevel: true,
-  title: "Comics",
-  style: ["FULLSCREEN"]
+    topLevel: true,
+    title: "Comics",
+    style: ["FULLSCREEN"]
 });
 
 var scrollView = tabris.create("ScrollView", {
-  layoutData: {left: 0, right: 0, top: 0, bottom: 0}
+    layoutData: {left: 0, right: 0, top: 0, bottom: 0}
 }).appendTo(page);
 
 var bgImageView = tabris.create("ImageView", {
-  layoutData: {left: 0, top: 0, right: 0}
+    layoutData: {left: 0, top: 0, right: 0}
 }).appendTo(scrollView);
 
 var titleTextView = tabris.create("TextView", {
-  layoutData: {left: 30, top: 30},
-  markupEnabled: true,
-  text: "The Muslim Show",
-  font: "32px sans-serif",
-  textColor: "white",
+    layoutData: {left: 30, top: 30},
+    markupEnabled: true,
+    text: "",
+    font: "32px sans-serif",
+    textColor: "white",
 }).appendTo(scrollView);
 
 var descTextView = tabris.create("TextView", {
-  layoutData: {left: 30, top: [titleTextView, 5]},
-  markupEnabled: true,
-  text: "<p>The Muslim Show Indonesia</p>",
-  textColor: "white",
+    layoutData: {left: 30, top: [titleTextView, 5]},
+    markupEnabled: true,
+    text: "",
+    textColor: "white",
 }).appendTo(scrollView);
-
-var contentComposite = tabris.create("Composite", {
-  layoutData: {left: 0, right: 0, top: [bgImageView, 0], height: 700},
-  background: "white"
-}).appendTo(scrollView);
-
-tabris.create("TextView", {
-  layoutData: {left: MARGIN, right: MARGIN, top: MARGIN},
-  text: "Etiam nisl nisi, egestas quis lacus ut, tristique suscipit metus. In vehicula lectus " +
-        "metus, at accumsan elit fringilla blandit. Integer et quam sed dolor pharetra " +
-        "molestie id eget dui. Donec ac libero eu lectus dapibus placerat eu a tellus. Fusce " +
-        "vulputate ac sem sit amet bibendum. Pellentesque euismod varius purus pharetra. Sed " +
-        "vitae ipsum sit amet risus vehicula euismod in at nunc. Sed in viverra arcu, id " +
-        "blandit. Praesent sagittis quis nisl id molestie. Donec dignissim, nisl id volutpat " +
-        "consectetur, massa diam aliquam lectus, sed euismod leo elit eu justo. Integer vel " +
-        "ante sapien.\n\nNunc sit amet blandit tellus, sed neque. Proin vel elementum augue. " +
-        "Quisque gravida nulla nisl, at fermentum turpis euismod in. Maecenas tortor at ante " +
-        "vulputate iaculis at vitae sem. Nulla dui erat, viverra eget mauris in, sodales " +
-        "mollis. Integer rhoncus suscipit mi in pulvinar. Nam metus augue, dictum a egestas " +
-        "ut, gravida eget ipsum. Nunc nisl, mollis et mauris in, venenatis blandit magna. " +
-        "Nullam scelerisque tellus lacus, in lobortis purus consectetur sed. Etiam pulvinar " +
-        "sapien vel nibh vehicula, in lacinia odio pharetra. Duis tincidunt metus a semper " +
-        "auctor. Sed nec consequat augue, id vulputate orci. Nunc metus nulla, luctus id " +
-        "porttitor nec, sed lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus."
-}).appendTo(contentComposite);
-
 
 scrollView.on("resize", function(widget, bounds) {
-  bgImageView.set("image", {src: "images/car-rent.jpg", width: bounds.width, height: bounds.width});
+    var xhr2 = new tabris.XMLHttpRequest();
+    xhr2.onreadystatechange = function() {
+        if (xhr2.readyState === xhr2.DONE) {
+            var dataCount = this.getResponseHeader('X-Pagination-Total-Count');
+            var models = JSON.parse(xhr2.responseText).data;
+            var collectionViewHeight = 100;
+
+            //content for collectionView comic chapters
+            var contentComposite = tabris.create("Composite", {
+                layoutData: {left: 0, right: 0, top: [bgImageView, 0], height: collectionViewHeight * dataCount},
+                background: "white"
+            }).appendTo(scrollView);
+
+            var i = dataCount;
+            //create comic chapters instances
+            tabris.create("CollectionView", {
+                layoutData: {left: 0, top: [bgImageView, 0]},
+                items: models,
+                itemHeight: collectionViewHeight,
+                initializeCell: function(cell) {
+                    var imageView = tabris.create("ImageView", {
+                        layoutData: {left: 0, top: 0, width: 100, height: 100}
+                    }).appendTo(cell);
+                    var titleTextView = tabris.create("TextView", {
+                        layoutData: {left: [imageView, 10], top: MARGIN_SMALL},
+                        font: "20px sans-serif",
+                    }).appendTo(cell);
+                    var numberTextView = tabris.create("TextView", {
+                        layoutData: {right: MARGIN_SMALL, top: MARGIN_SMALL},
+                        font: "20px sans-serif",
+                    }).appendTo(cell);
+                    var dateTextView = tabris.create("TextView", {
+                        layoutData: {left: [imageView, 10], top: [titleTextView, MARGIN_SMALL]},
+                        font: "12px sans-serif",
+                    }).appendTo(cell);
+                    cell.on("change:item", function(widget, model) {
+                        imageView.set("image", {src: model.image});
+                        titleTextView.set("text", model.title);
+                        numberTextView.set("text", "#" + i);
+                        dateTextView.set("text", moment(model.createdAt).format('ll'));
+                        i--;
+                    });
+                }
+            }).on("select", function(target, obj) {
+                createViewPage(obj).open();
+            }).appendTo(contentComposite);
+        }
+    };
+    xhr2.open("GET", "http://128.199.228.15:5001/api/v1/categories/the-muslim-show/catalogs/?key=a110568402c460bb91f2695d4052fe7b9fe6cb26");
+    xhr2.send();
+
+    var xhr = new tabris.XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === xhr.DONE) {
+            var model = JSON.parse(xhr.responseText).data;
+            var image = encodeURI(model.image.trim());
+
+            titleTextView.set("text", model.title);
+            descTextView.set("text", model.seoDescription);
+            bgImageView.set("image", {src: image, width: bounds.width, height: bounds.width});
+        }
+    };
+    xhr.open("GET", "http://128.199.228.15:5001/api/v1/categories/the-muslim-show/?key=a110568402c460bb91f2695d4052fe7b9fe6cb26");
+    xhr.send();
 });
 
 scrollView.on("scroll", function(widget, offset) {
-  bgImageView.set("transform", {translationY: offset.y * 0.4});
+    bgImageView.set("transform", {translationY: offset.y * 0.4});
 });
 
 page.open();
+
+function createViewPage(obj) {
+    var page = tabris.create("Page", {
+        title: obj.title
+    });
+
+    var scrollView = tabris.create("ScrollView", {
+        layoutData: {left: 0, right: 0, top: 0, bottom: 0}
+    }).appendTo(page);
+
+    //create footer
+    var footerComposite = tabris.create('Composite', {
+        layoutData: {bottom: 0, left: 0, right: 0, height: 50},
+        background: "black"
+    }).appendTo(page);
+    tabris.create("TextView", {
+        layoutData: {top: [footerComposite, 10], left: 0},
+        text: "test",
+        textColor: "white"
+    }).appendTo(footerComposite);
+
+    scrollView.on("resize", function(widget, bounds) {
+        var xhr = new tabris.XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === xhr.DONE) {
+                var models = JSON.parse(xhr.responseText).data;
+
+                //content for collectionView comic pages
+                var contentComposite = tabris.create("Composite", {
+                    layoutData: {left: 0, right: 0, top: 0, bottom: 0},
+                    background: "white"
+                }).appendTo(scrollView);
+
+                //display comic image to webview
+                tabris.create("WebView", {
+                    layoutData: {left: 0, top: 0, right: 0, bottom: [footerComposite, 10]},
+                    url: models[0].image,
+                }).appendTo(contentComposite);
+            }
+        };
+        xhr.open("GET", "http://128.199.228.15:5001/api/v1/catalogs/" + obj._id + "/photos/?key=a110568402c460bb91f2695d4052fe7b9fe6cb26&limit=1");
+        xhr.send();
+    });
+
+    return page;
+}
       
